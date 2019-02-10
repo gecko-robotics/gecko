3a0b577a3ebc20e97f98a4b16cc95e85aa30be12
walchko <walchko@users.noreply.github.com>
2019-02-10 14:15:02 -0500

    up

--- src/msocket.hpp
+++ src/msocket.hpp
@@ -4,6 +4,10 @@
 #include <stdint.h>
 #include <string>
 
+// http://www.kohala.com/start/mcast.api.txt
+// http://www.kohala.com/start/
+// https://rg3.name/201504241907.html
+
 class SSocket{
 public:
     void init(std::string mc_addr_str={"224.3.29.110"}, uint16_t mc_port=11311, uint8_t mc_ttl=1);
