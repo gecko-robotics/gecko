c58e6045cd45a384471373b54be102b91d82ca79
walchko <walchko@users.noreply.github.com>
2019-02-10 16:53:15 -0500

    up :taco: :taco:

--- msgpack/gecko.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "msgs.h"
-#include <string>
-
-// class ReplyMsg: public base_t {
-// public:
-//     ReplyMsg(std::string& msg): base_t(GREPLY)
-// }
--- src/gecko.hpp
+++ src/gecko.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
+// These are the headers a user would access. Others are for internal
+// use only.
 // #include "geckocpp.hpp"
-#include "time.hpp"
-#include "node.hpp"
-// #include "signals.hpp"
-#include "transport.hpp"
+#include "time.hpp"        // rate
+#include "node.hpp"        // threads
+#include "transport.hpp"   // pub-sub
+#include "helpers.hpp"     // tcp://x.x.x.x:port
+#include "network.hpp"     // ip hostname
+#include "exceptions.hpp"  // NotImplemented
+#include "color.hpp"       // ColorPrint
 
 // msgpack stuff
 #include <msgpack.hpp>
-#include "msgs.hpp"
-#include "serialization.hpp"
+#include "msgs.hpp"  // includes all message headers
 
 // copy header from here
 // https://github.com/zeromq/cppzmq
