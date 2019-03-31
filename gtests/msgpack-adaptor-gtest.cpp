adc51b1160b82d2541355e7e062a998b8904302d
walchko <walchko@users.noreply.github.com>
2019-03-31 17:57:31 -0400

    update

---
+++ gtests/msgpack-adaptor-gtest.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h> // googletest header file
+#include "gecko.hpp"
+#include "msgs.hpp"
+#include "msgpack_pub_sub.hpp"
+#include <string>
+
+using namespace std;
+
+TEST(msgpack, msg_adaptor) {
+    string uds = zmqUDS("/tmp/0");
+
+    vec_t a(1,-2,3.3);
+
+    Publisher *p = new Publisher(uds);
+    mpPublisher<vec_t> pub(p);
+    pub.publish(a);
+
+    Subscriber *s = new Subscriber(uds);
+    mpSubscriber<vec_t> sub(s);
+    vec_t b = sub.recv_nb();
+
+    a.print();
+    b.print();
+
+    ASSERT_TRUE(a == b);
+}
