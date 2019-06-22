f5301e5f93cf8fe13d11a07a75a3bd158e4924be
walchko <walchko@users.noreply.github.com>
2019-06-22 07:53:51 -0400

    Update opencv-test.cpp

--- dev/opencv/opencv-test.cpp
+++ dev/opencv/opencv-test.cpp
@@ -3,8 +3,21 @@
 #include <opencv2/opencv.hpp>
 #include <stdint.h>
 
+#include <optional>
+#include <any>
+
 using namespace cv;
 using namespace std;
+
+optional<int> convert(const string& s) {
+  try {
+	int res = stoi(s);
+	return res;
+  }
+  catch(exception&) {
+	return {};
+  }
+}
 
 int main(){
     Mat img(100, 100, CV_8UC1);
@@ -16,6 +29,30 @@ int main(){
     // int i = 0;
     // for (auto const& v: buf) cout << " " << int(v);
     // cout << endl;
+
+    Mat img2 = imdecode(buf, IMREAD_GRAYSCALE);
+    cout << img2.size() << endl;
+
+    int v = convert("123").value_or(0);
+    cout << v << endl;
+
+    int v1 = convert("abc").value_or(0);
+    cout << v1 << endl;
+
+    vector<any> vv { 1, 2.2, false, "hi!" };
+
+    auto& t = vv[1].type();  // What is being held in this any?
+    if (t == typeid(double))
+        cout << "We have a double" << "\n";
+    else
+        cout << "We have a problem!" << "\n";
+
+    cout << any_cast<double>(vv[1]) << endl;
+    try {
+        cout << any_cast<int>(vv[1]) << endl;
+    } catch(bad_any_cast&) {
+        cout << "wrong type" << endl;
+    }
 
     return 0;
 }
