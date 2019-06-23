cca083acbbdbe7613cbf58fd54d63f971b5fb02c
walchko <walchko@users.noreply.github.com>
2019-06-23 19:55:08 -0400

    Update camera-test.cpp

--- dev/opencv/camera-test.cpp
+++ dev/opencv/camera-test.cpp
@@ -8,19 +8,19 @@
 class b_t: public base_t {
 public:
     b_t(): base_t(31) {}
-    b_t(const cv::Mat img): base_t(31), width(img.cols), height(img.rows) {
+    b_t(const cv::Mat img): base_t(31), width(img.cols), height(img.rows), channels(img.channels()) {
         cv::imencode(".jpg", img, buffer);
     }
-    b_t(const b_t& a): base_t(31), width(a.width), height(a.height), buffer(a.buffer) {}
+    b_t(const b_t& a): base_t(31), width(a.width), height(a.height), buffer(a.buffer), channels(a.channels) {}
     bool operator==(const b_t& v) const {
-        if((width == v.width) && (height == v.height) && (buffer == v.buffer))
+        if((width == v.width) && (height == v.height) && (buffer == v.buffer) && (channels == v.channels))
             return true;
         return false;
     }
-    cv::Mat get(){return cv::imdecode(buffer, cv::IMREAD_GRAYSCALE);}
-    uint32_t width, height;
+    cv::Mat get(){return cv::imdecode(buffer, channels == 1 ? cv::IMREAD_GRAYSCALE : cv::IMREAD_COLOR);}
+    uint32_t width, height, channels;
     std::vector<uint8_t> buffer;
-    MSGPACK_DEFINE(width, height, buffer);
+    MSGPACK_DEFINE(width, height, channels, buffer);
     GECKO_MSG(b_t, 31);
 };
 
@@ -28,7 +28,7 @@ using namespace std;
 using namespace cv;
 
 int main(){
-    Mat img(100, 100, CV_8UC1);
+    Mat img(100, 100, CV_8UC3);
     b_t b(img);
     zmq::message_t m = b.pack();
     b_t c(m);
