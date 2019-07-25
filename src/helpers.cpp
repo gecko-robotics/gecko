a399a24f7ad9c8d186a45379bf2259f4d8037a1f
walchko <walchko@users.noreply.github.com>
2019-07-24 20:05:53 -0400

    Update helpers.cpp

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,12 +1,15 @@
-#include <helpers.hpp>
+#include <gecko/helpers.hpp>
 #include <pwd.h>        // geteuid,getpwuid
 #include <sys/types.h>  // umask, getpid
 #include <sys/stat.h>   // umask
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 
 char *getUserName(void)
 {
-    char * username;
+    char * username = nullptr;
     // - getpwuid() function shall search the user database
     //    for an entry with a matching uid
     // - geteuid() function shall return the effective user
@@ -18,11 +21,11 @@ char *getUserName(void)
     {
         username = strdup(pw->pw_name);
     }
-    else
-    {
-        if(asprintf(&username, "%i", euid) < 0)
-            return NULL;
-    }
+    // else
+    // {
+    //     if(asprintf(&username, "%i", euid) < 0)
+    //         return nullptr;
+    // }
 
     return username;
 }
