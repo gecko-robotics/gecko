537fc5021e50d11784d438b79857b2a4370f57b9
walchko <walchko@users.noreply.github.com>
2019-09-29 13:44:53 -0400

    update

---
+++ build_extlibs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+mkdir tmp
+cd tmp
+
+DIR="/opt/gecko"
+
+sudo mkdir ${DIR}
+sudo chown -R ${USER}:${USER} ${DIR}
+
+# MsgPack ----------------------------------------------------------
+echo ">> Installing MsgPack in ${DIR}"
+git clone https://github.com/msgpack/msgpack-c.git
+mkdir msgpack-c/build
+cd msgpack-c/build
+cmake -DMSGPACK_CXX[17]=ON -DCMAKE_INSTALL_PREFIX=${DIR} ..
+make install
+cd ../..
+# rm -fr msgpack-c
+
+# json ---------------------------------------------------------------
+echo ">> Installing JSON in ${DIR}"
+git clone https://github.com/nlohmann/json.git
+mkdir json/build
+cd json/build
+cmake -DCMAKE_INSTALL_PREFIX=${DIR} -DJSON_BuildTests=OFF ..
+make install
+cd ../..
+# rm -fr json
+
+# cppzmq --------------------------------------------------------------
+echo ">> Installing cppzmq in ${DIR}"
+git clone https://github.com/zeromq/cppzmq.git
+mkdir cppzmq/build
+cd cppzmq/build
+cmake -DCMAKE_INSTALL_PREFIX=${DIR} -DCPPZMQ_BUILD_TESTS=OFF ..
+make install
+cd ../..
+# rm -fr cppzmq
+
+cd ..
+# rm -fr tmp
