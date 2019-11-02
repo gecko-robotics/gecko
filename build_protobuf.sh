f0ee37e73deb507a894efc1338f8c6d7d4c45bbd
walchko <walchko@users.noreply.github.com>
2019-11-02 13:48:19 -0400

    Delete build_protobuf.sh

--- build_protobuf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p ./python/pygecko/proto
-# protoc -I=../proto --python_out=./proto ../proto/*.proto
-# protoc --proto_path=. --python_out=python/pygecko pygecko/proto/*.proto
-# protoc --proto_path=. --cpp_out=build proto/*.proto
-# touch ./proto/__init__.py
-# protoc --proto_path=extlibs/protobuf --cpp_out=extlibs/protobuf extlibs/protobuf/*.proto
-protoc --proto_path=. --python_out=python/pygecko single/msgs.proto
