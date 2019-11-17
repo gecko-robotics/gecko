8d5e8fdebe6488fbc5bab484e64c665cf4e9bf61
walchko <walchko@users.noreply.github.com>
2019-11-16 22:13:05 -0500

    fix github workflow? :tada: :bento:

--- cpp/cmake/msgpack.cmake
+++ cpp/cmake/msgpack.cmake
@@ -1,49 +1,49 @@
-if (NOT APPLE)
-    set(msgpack_DIR $ENV{HOME}/.local/lib/cmake/msgpack)
-    include_directories($ENV{HOME}/.local/include)
-    link_directories($ENV{HOME}/.local/lib)
-else()
-    # include_directories($ENV{HOME}/github/gecko/src)
-    # include_directories($ENV{HOME}/github/gecko/zmq)
-    # link_directories($ENV{HOME}/github/gecko/build)
-    include_directories(/usr/local/include)
-    link_directories(/usr/local/lib)
-endif()
-
-# Get msgpack -----------------------------------------------------
-if(APPLE)
-  pkg_check_modules(MessagePack REQUIRED msgpack)
-  if(MessagePack_FOUND)
-      include_directories(${MessagePack_INCLUDE_DIR})
-      message(STATUS ">> MsgPack Found: ${MessagePack_VERSION}")
-      message(STATUS ">> MsgPack: ${MessagePack_LIBRARIES}  ${MessagePack_INCLUDE_DIR}")
-      list(APPEND ALL_LIBS ${ALL_LIBS} ${MessagePack_LIBRARIES})
-
-      # this is a template library, so no cpp
-      # include_directories(msgpack)
-      include_directories(.)
-
-  else()
-      message(WARNING "\n<<--- Could not find MsgPack --->>\n")
-  endif()
-elseif(UNIX)
-    find_package(msgpack REQUIRED)
-    if(msgpack_FOUND)
-        include_directories(${msgpack_INCLUDE_DIR})
-        message(STATUS ">> MsgPack Found: ${msgpack_VERSION}")
-        message(STATUS ">> MsgPack: ${msgpack_LIBS}  ${msgpack_INCLUDE_DIR}")
-        list(APPEND ALL_LIBS ${msgpack_LIBRARIES})
-
-        # this is a template library, so no cpp
-        # include_directories(msgpack)
-        include_directories(${CMAKE_CURRENT_LIST_DIR}/../.)
-
-    else()
-        message(WARNING "\n<<--- Could not find MsgPack --->>\n")
-    endif()
-endif()
-
-message(STATUS ">> Compiling MsgPack")
-# message(STATUS "oo> ${PATH}")
-include_directories(${CMAKE_CURRENT_LIST_DIR}/../extlibs/gecko-msgpack)
-# add_subdirectory(${PATH}/extlibs/gecko-msgpack)
+# if (NOT APPLE)
+#     set(msgpack_DIR $ENV{HOME}/.local/lib/cmake/msgpack)
+#     include_directories($ENV{HOME}/.local/include)
+#     link_directories($ENV{HOME}/.local/lib)
+# else()
+#     # include_directories($ENV{HOME}/github/gecko/src)
+#     # include_directories($ENV{HOME}/github/gecko/zmq)
+#     # link_directories($ENV{HOME}/github/gecko/build)
+#     include_directories(/usr/local/include)
+#     link_directories(/usr/local/lib)
+# endif()
+#
+# # Get msgpack -----------------------------------------------------
+# if(APPLE)
+#   pkg_check_modules(MessagePack REQUIRED msgpack)
+#   if(MessagePack_FOUND)
+#       include_directories(${MessagePack_INCLUDE_DIR})
+#       message(STATUS ">> MsgPack Found: ${MessagePack_VERSION}")
+#       message(STATUS ">> MsgPack: ${MessagePack_LIBRARIES}  ${MessagePack_INCLUDE_DIR}")
+#       list(APPEND ALL_LIBS ${ALL_LIBS} ${MessagePack_LIBRARIES})
+#
+#       # this is a template library, so no cpp
+#       # include_directories(msgpack)
+#       include_directories(.)
+#
+#   else()
+#       message(WARNING "\n<<--- Could not find MsgPack --->>\n")
+#   endif()
+# elseif(UNIX)
+#     find_package(msgpack REQUIRED)
+#     if(msgpack_FOUND)
+#         include_directories(${msgpack_INCLUDE_DIR})
+#         message(STATUS ">> MsgPack Found: ${msgpack_VERSION}")
+#         message(STATUS ">> MsgPack: ${msgpack_LIBS}  ${msgpack_INCLUDE_DIR}")
+#         list(APPEND ALL_LIBS ${msgpack_LIBRARIES})
+#
+#         # this is a template library, so no cpp
+#         # include_directories(msgpack)
+#         include_directories(${CMAKE_CURRENT_LIST_DIR}/../.)
+#
+#     else()
+#         message(WARNING "\n<<--- Could not find MsgPack --->>\n")
+#     endif()
+# endif()
+#
+# message(STATUS ">> Compiling MsgPack")
+# # message(STATUS "oo> ${PATH}")
+# include_directories(${CMAKE_CURRENT_LIST_DIR}/../extlibs/gecko-msgpack)
+# # add_subdirectory(${PATH}/extlibs/gecko-msgpack)
--- cpp/cmake/zmq.cmake
+++ cpp/cmake/zmq.cmake
@@ -1,14 +1,18 @@
-pkg_check_modules(libzmq REQUIRED libzmq)
-if(libzmq_FOUND)
-    message(STATUS ">> ZeroMQ found: ${libzmq_VERSION}")
+# pkg_check_modules(libzmq REQUIRED libzmq)
+find_package(ZeroMQ REQUIRED
+    PATHS /opt/gecko/share/cmake
+)
+# if(libzmq_FOUND)
+if (ZeroMQ_FOUND)
+    message(STATUS ">> ZeroMQ found: ${ZeroMQ_VERSION}")
     # add_compile_definitions(ZMQ_BUILD_DRAFT_API)
     # add_compile_definitions(ZMQ_CPP11)
-    include_directories(${libzmq_INCLUDE_DIRS})
+    # include_directories(${ZeroMQ_INCLUDE_DIR})
     # message(STATUS "zmq: ${libzmq_INCLUDE_DIRS}")
     # list(APPEND ALL_LIBS ${ALL_LIBS} ${libzmq_LIBRARIES})
-    list(APPEND ALL_LIBS ${libzmq_LIBRARIES})
+    # list(APPEND ALL_LIBS ${ZeroMQ_LIBRARY})
 
-    option(ZMQ_BUILD_DRAFT_API " " OFF)
+    # option(ZMQ_BUILD_DRAFT_API " " OFF)
 
     # ZeroMQ CPP ------------------------------------------------------
     # git submodule add https://github.com/zeromq/cppzmq
