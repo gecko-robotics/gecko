e4b14b28a15e683ffe2d54eb7731fc38287cd4d5
walchko <walchko@users.noreply.github.com>
2019-11-30 10:22:31 -0500

    version.hpp update

--- cpp/tools/version/version.cpp
+++ cpp/tools/version/version.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include <iomanip>
+#include <gecko/version.hpp>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
     std::cout << std::setw(6) << argv[0] << " cpp: " <<  __cplusplus << std::endl;
+    cout << "gecko version: " << GECKO_VERSION << endl;
 
     return 0;
 }
